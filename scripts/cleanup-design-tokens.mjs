e26d232f8147dd15b648effd641670671946d7a4
Janis Koselevs <janis.koselevs@accenture.com>
2025-03-25 21:21:38 +0200

    feat: adding some useful links

--- scripts/cleanup-design-tokens.mjs
+++ scripts/cleanup-design-tokens.mjs
@@ -1,6 +1,6 @@
 import { unlink } from 'node:fs';
 
-unlink('tokens.json', function(error) {
+unlink('./tokens.json', function(error) {
   if(error) return console.error(error);
   console.log('tokens.json file deleted successfully')
 });
